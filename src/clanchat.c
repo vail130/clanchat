86270031cabad3565ff7444e896e14d046a9ea37
Vail Gold <vail130@gmail.com>
2016-01-17 02:11:55 -0500

    Fetch local network IPs using arp

--- src/clanchat.c
+++ src/clanchat.c
@@ -1,8 +1,10 @@
 #include <errno.h>
+#include <regex.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
@@ -11,12 +13,150 @@
 #include "clanchat.h"
 #include "clanchatapi.h"
 
+#define BUFSIZE 4096    /* Should be enough to capture a whole line of output from "arp -a" */
+
 pid_t pid;
 
 volatile sig_atomic_t done = 0;
  
 void term(int signum) {
     kill(pid, SIGTERM);
+}
+
+void register_sigterm_handler() {
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = term;
+    sigaction(SIGTERM, &action, NULL);
+}
+
+int load_arp_output(char ** arp_output) {
+    FILE *fp;
+    fp = popen("arp -a", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Call to \"arp -a\" failed.");
+        return EXIT_FAILURE;
+    }
+
+    char buf[BUFSIZE];
+    if (*arp_output == NULL) {
+        *arp_output = malloc(0);
+    }
+
+    while (fgets(buf, BUFSIZE, fp) != NULL) {
+        *arp_output = realloc(*arp_output, sizeof(char) * (strlen(*arp_output) + strlen(buf)));
+        strcat(*arp_output, buf);
+    }
+    
+    if(pclose(fp))  {
+        fprintf(stderr, "Error closing pipe to shell.");
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int extract_ip_addresses_from_arp_output(char * arp_output, char ** ip_address_list) {
+    regex_t regex;
+    const char * ipv4_regex_pattern = "([0-9]{1,3}(\\.[0-9]{1,3}){3})";
+    int reti = regcomp(&regex, ipv4_regex_pattern, REG_EXTENDED);
+    if (reti != 0) {
+        fprintf(stderr, "Could not compile regex\n");
+        return EXIT_FAILURE;
+    }
+
+    /* Use capturing group */
+    size_t ngroups = regex.re_nsub + 1;
+    regmatch_t *groups = malloc(ngroups * sizeof(regmatch_t));
+
+    char * arp_output_line = malloc(sizeof(arp_output));
+    char * arp_remaining = malloc(sizeof(arp_output));
+
+    int ret_status = EXIT_SUCCESS;
+    while (1) {
+        if (strlen(arp_output) == 0) {
+            break;
+        }
+
+        char * result;
+        result = strstr(arp_output, "\n");
+        result = result + sizeof(char); /* Move forward one character: "\n" */
+        if (result == NULL || strlen(result) == 0) {
+            break;
+        }
+      
+        /* Copy result substring into separate memory space to avoid conflicts */
+        arp_remaining = realloc(arp_remaining, sizeof(char) * strlen(result));
+        strcpy(arp_remaining, result);
+
+        /* Reallocate and clear memory for next output line. Clearing memory is necessary for strlen to work properly. */
+        arp_output_line = realloc(arp_output_line, sizeof(char) * (strlen(arp_output) - strlen(arp_remaining)));
+        memset(arp_output_line, 0, sizeof(char) * strlen(arp_output_line));
+        strncpy(arp_output_line, arp_output, strlen(arp_output) - strlen(arp_remaining));
+        
+        /* Update arp_output for next iteration. Clear memory before copying so strlen works properly. */
+        arp_output = realloc(arp_output, sizeof(char) * strlen(arp_remaining));
+        memset(arp_output, 0, sizeof(char) * strlen(arp_output));
+        strcpy(arp_output, arp_remaining);
+        
+        /*
+         * "? (192.168.0.1) at 14:cf:e2:ab:a7:97 on en0 ifscope [ethernet]"
+         * "? (192.168.33.255) at (incomplete) on vboxnet0 ifscope [ethernet]"
+         */
+
+        reti = regexec(&regex, arp_output_line, ngroups, groups, 0);
+        if (reti != 0) {
+            continue;
+        }
+        
+        char ip[15]; /* IPv4 string is max 15 characters */
+        strncpy(ip, arp_output_line + groups[1].rm_so, groups[1].rm_eo - groups[1].rm_so);
+        
+        printf("Matching IP: %s\n", ip);
+        
+        if (strlen(*ip_address_list) == 0) {
+            *ip_address_list = realloc(*ip_address_list, sizeof(char) * strlen(ip));
+            strcpy(*ip_address_list, ip);
+        } else {
+            *ip_address_list = realloc(*ip_address_list, (sizeof(char) * strlen(*ip_address_list)) + (sizeof(char) * (strlen(ip)+1)));
+            strcat(*ip_address_list, ",");
+            strcat(*ip_address_list, ip);
+        }
+    }
+
+    free(arp_remaining);
+    free(arp_output_line);
+    free(groups);
+    regfree(&regex);
+
+    return ret_status;
+}
+
+int register_lan_clients() {
+    /* Allocate everything up here so we can safely clean up everything using goto statement */
+    int ret_status = EXIT_SUCCESS;
+    char * arp_output = malloc(0);
+    char * ip_address_list = malloc(0);
+    
+    if ((load_arp_output(&arp_output)) != 0) {
+        fprintf(stderr, "Failed to load output for command \"arp -a\"");
+        ret_status = EXIT_FAILURE;
+        goto cleanup_register_lan_clients;
+    }
+
+    if ((extract_ip_addresses_from_arp_output(arp_output, &ip_address_list)) != 0) {
+        fprintf(stderr, "Failed to extract IP addresses from arp output.");
+        ret_status = EXIT_FAILURE;
+        goto cleanup_register_lan_clients;
+    }
+
+    /* TODO: Connect to clients with IP addresses and register them */
+
+    cleanup_register_lan_clients:
+        free(arp_output);
+        free(ip_address_list);
+
+    return ret_status;
 }
 
 int clanchat(ClanchatConfig opts) {
@@ -29,38 +169,42 @@ int clanchat(ClanchatConfig opts) {
         fprintf(stderr, "ID must not be empty.\n");
         return EXIT_FAILURE;
     }
-
-    if (opts.mode == CLIENT_MODE) {
-        pid = fork();
-        if (pid == 0) {
-            // Child process
-            ClanchatConfig server_opts = { opts.name, opts.id, SERVER_MODE };
-            return clanchat(server_opts);
-        } else if (pid < 0) {
-            // Failure
-            fprintf(stderr, "Fork failure.");
-            return EXIT_FAILURE;
-        } else {
-            // Parent process
-            
-            // Register sigterm handler to cleanup child
-            struct sigaction action;
-            memset(&action, 0, sizeof(struct sigaction));
-            action.sa_handler = term;
-            sigaction(SIGTERM, &action, NULL);
-
-            // TODO
-            int status;
-            wait(&status);
-            return status;
-        }
-    } else if (opts.mode == SERVER_MODE) {
-        return clanchat_api_serve(opts);
-    } else {
+    
+    if (opts.mode != CLIENT_MODE && opts.mode != SERVER_MODE) {
         fprintf(stderr, "Invalid mode: \"%d\"\n", opts.mode);
         return EXIT_FAILURE;
     }
 
+    if (opts.mode == SERVER_MODE) {
+        return clanchat_api_serve(opts);
+    }
+    
+    /* CLIENT_MODE */
+    pid = fork();
+    if (pid == 0) {
+        /* Child process */
+        ClanchatConfig server_opts = { opts.name, opts.id, SERVER_MODE };
+        return clanchat(server_opts);
+    } else if (pid < 0) {
+        /* Failure */
+        fprintf(stderr, "Fork failure.");
+        return EXIT_FAILURE;
+    }
+    
+    /* Parent process */
+    register_sigterm_handler();
+    register_lan_clients();
+    
+    /* TODO: Read commands from stdin and parse them */
+    
+    /*
+     * help: Display commands and descriptions
+     * users: List available users
+     * msg [USER]: Send a message to a user
+     */
+
+    while (1) {}
+    
     return EXIT_SUCCESS;
 }
 
